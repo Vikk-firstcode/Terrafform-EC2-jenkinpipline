5b39cd3d8815422cf164787d1a431865cbaa3ee9
Vikk-firstcode <yashaery@gmail.com>
2024-10-02 23:00:57 +0530

    Update install-Terrform_ansible.sh

--- install-Terrform_ansible.sh
+++ install-Terrform_ansible.sh
@@ -28,3 +28,10 @@ apt install unzip
 unzip awscliv2.zip
 sudo ./aws/install
 aws configure
+
+#step to install Terraform 
+#link -- https://developer.hashicorp.com/terraform/install#linux
+
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
